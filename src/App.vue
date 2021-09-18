661b936bc7b5b53c6c8c7f2fd76c428bbc1abf5b
thomas-gleizes <thomasgleizes34@gmail.com>
2021-09-18 16:49:51 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,27 @@
       <Folder name="code" />
       <File name="test.js" />
     </div>
+
+    <div class="max-w-400 mx-10">
+      <Card title="Timeline" />
+      <Card title="Metadata" />
+    </div>
   </main>
 </template>
 
 <script>
 import Folder from "./components/common/Folder.vue";
-import File from "./components/common/File.vue"
+import File from "./components/common/File.vue";
 
 import Header from "@/components/layout/Header";
+import Card from "./components/layout/Card";
 
 export default {
   name: "App",
   components: {
-    Folder, File, Header,
-  },
+    Card,
+    Folder, File, Header
+  }
 };
 </script>
 
