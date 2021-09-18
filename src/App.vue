68d39071c39ff05aa3e3e663f9b9c8e8e7cd4465
thomas <thomasgleizes34@gmail.com>
2021-09-18 12:48:41 +0200

    add fontawsone

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
   <div class="w-screen">
     <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
     <h1 class="text-3xl font-bold text-green-500">Welcome to Your Vue.js App</h1>
+    <div class="flex">
+      <Folder name="app" />
+      <Folder name="app" />
+    </div>
   </div>
 </template>
 
 <script>
+import Folder from "./components/Folder.vue";
+
 export default {
   name: "App",
+  components: {
+    Folder,
+  },
 };
 </script>
 
---
+++ src/components/Folder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border m-2 p-4 rounded">
+    <div>
+      <fa icon="folder" size="6x" />
+    </div>
+    <div>{{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Folder",
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: null,
+    },
+  },
+};
+</script>
