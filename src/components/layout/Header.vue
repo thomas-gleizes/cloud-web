6806376db890b3a831390d07876416bd0719bcca
thomas-gleizes <thomasgleizes34@gmail.com>
2021-09-23 15:52:06 +0200

    add clock to header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,26 @@
 <script>
 import TabsBar from "@/components/layout/navigation/TabsBar";
+import Clock from "../common/Clock";
 
 export default {
   name: "Header",
-  components: { TabsBar }
+  components: { TabsBar, Clock },
 };
 </script>
 
 <template>
   <header>
-    <div class="flex justify-start py-2 px-20 bg-gray-50 shadow-md border w-screen">
-      <div class="mx-5">
-        <img width="60" alt="Vue logo" src="../../assets/logo.png" />
-      </div>
+    <div class="flex justify-between py-2 px-3 bg-gray-50 shadow-md border w-screen">
+      <div class="flex justify-start">
+        <div class="mx-5">
+          <img width="60" alt="Vue logo" src="../../assets/logo.png" />
+        </div>
 
-      <h2 class="text-2xl mx-5 font-bold text-green-600 italic my-auto">Cloud web</h2>
+        <h2 class="text-2xl mx-5 font-bold text-green-600 italic my-auto">Cloud web</h2>
+      </div>
+      <div class="flex justify-end my-auto text-lg">
+        <Clock />
+      </div>
     </div>
   </header>
   <tabs-bar />
